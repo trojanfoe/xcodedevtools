b735a2b6eb0966ffbbd27bdbdc304c52d10b22b5
SchlaubiSchlump <Schlaubischlump@users.noreply.github.com>
2019-09-10 21:19:01 +0200

    Add __future__ import for print

--- bump_buildnum.py
+++ bump_buildnum.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 #
 # Bump build number in Info.plist files if a source file have changed.
 #
--- copy_dylibs.py
+++ copy_dylibs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 """Copy dylibs into the build folder.
 
